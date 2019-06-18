db2d4e90e2cf30465c1c0dc675dc7864e1744715
natashasok <nataffka.sokolova@gmail.com>
2019-06-18 20:34:12 +0300

    подключила testng нужной версии

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.ptf.sandbox.Square;
+
+import static org.testng.Assert.assertEquals;
+
+public class SquareTests {
+
+    @Test
+
+ public void testArea() {
+        Square s= new Square(5);
+        Assert.assertEquals(s.area(),25.0);
+    }
+}
